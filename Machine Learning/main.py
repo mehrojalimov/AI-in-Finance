940450107601ed03237d324fa877ec727b062de2
Mehroj Alimov <alimovmekhroj01@gmail.com>
2025-02-21 15:57:53 -0500

    adding the necessary packages

---
+++ Machine Learning/main.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import seaborn as sns
+import matplotlib.pyplot as plt
+sns.set_theme()
+plt.style.use('seaborn-v0_8-darkgrid')
+mpl.rcParams['savefig.dpi'] = 300
+mpl.rcParams['font.family'] = 'serif'
+np.set_printoptions(precision=4, suppress=True)
